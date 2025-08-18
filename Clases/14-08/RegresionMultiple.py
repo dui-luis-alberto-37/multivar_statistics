384fc698e5ef4f08301bca8bf792e260d29b2530
lgarcia <lgarciaorozco6@gmail.com>
2025-08-18 01:01:31 -0600

    RM

--- Clases/14-08/RegresionMultiple.py
+++ Clases/14-08/RegresionMultiple.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 class RM():
     def __innit__(self, data:pd.DataFrame, yname:str):
         self.data = data
@@ -11,5 +13,27 @@ class RM():
 
 
     def _calculate_params(self):
-        Y = np.array(self.y)
-        X = np.array(self.x)
+        Y = np.array(self.y).T
+        X = self.x
+        # X = np.array(self.x)
+        n = len(X)
+
+
+
+        nX = [[1] for _ in range(n)]
+        for i in range(n):
+            nX[i] += list(X.iloc[i])
+        
+        X = np.array(nX)
+        Xt = X.T                        # * Matrices transpuestas
+        Yt = Y.T
+
+        beta = np.linalg.inv(Xt@X)@Xt@Y
+        SSE = Yt@Y -beta.T@X.T@Y
+
+        p = len(beta)
+        gl = n-p
+
+        varest = SSE / gl
+
+        return beta, varest
