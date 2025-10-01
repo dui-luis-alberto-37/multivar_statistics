0cd56f4db5d440c3e29f1665350dbb0906419faf
lgarcia <lgarciaorozco6@gmail.com>
2025-09-30 18:23:43 -0600

    HW::analis_varianza_1.1

---
+++ homework/analisis_varianza/2-8-1.R
@@ -0,0 +1,29 @@
+setwd("../../homework/analisis_varianza/")
+source("../../mylibrary/RegresionMultiple.R")
+datos = datasets::trees
+datos
+
+#1)
+mc = lm('Volume ~ .', datos); mc
+
+summary(mc)
+anov_mc = anova_table(mc); anov_mc
+
+
+m1 = lm('Volume ~ Girth', datos)
+m1
+
+summary(m1)
+anov_m1 = anova_table(m1); anov_m1
+
+
+anova(m1,mc)
+anova(mc,m1)
+anova(mc)
+anova(m1)
+
+#2)
+m2 = lm('Volume ~ 1', datos)
+m2
+
+anov$`F value`
